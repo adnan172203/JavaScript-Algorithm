82fd7584e139fefb2b193df39a7169191e49ca14
Adnan <adnan172203@gmail.com>
2019-10-17 21:47:22 +0600

    add two algorithm, reverse string and palindrome

---
+++ 1.reverse_string/solution2.js
@@ -0,0 +1,18 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+//------------------------------------------------------
+// --- Examples
+//   reverse('apple') === 'elppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+
+function reverse(str) {
+  let reversed = '';
+  
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+  return reversed;
+}
